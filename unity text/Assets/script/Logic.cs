fb7621c2ee15db2f739240a435d2bb9df3c7986d
clairehoyt <clairehoyt@users.noreply.github.com>
2015-06-09 16:28:58 +0800

    add light and tween color

---
+++ unity text/Assets/script/Logic.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Logic : MonoBehaviour {
+		public GameObject cube;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
